171f5f6b7586c67805cde04a02e1220ea0b465ef
David Turner <cillian64@googlemail.com>
2014-01-28 20:07:43 +0000

    Significant refactor and bugfix

--- loraine/clients/bookshelf_colour_fade.py
+++ loraine/clients/bookshelf_colour_fade.py
@@ -24,52 +24,29 @@ for i in range(0,len(shelves)):
 	shelf_colours.append((0,0,0))
 
 set_shelves()
-	
-step=10
-Rstep, Gstep, Bstep = 1, 1, 1
-R,G,B = 0,0,0
+
+RGB=list((0, 0, 0))
+shelf_colours[0]=RGB
+steps=list((1,1,1))	
 while True:
-	if random.random()>0.999:
-		Rstep = -Rstep
-	R += Rstep
+	for i in range(0,3):
+		if random.random()>0.999:
+			steps[i] = -steps[i]
+		RGB[i] += steps[i]
 
-	if random.random()>0.999:
-		Gstep = -Gstep
-	G += Gstep
+		if RGB[i] > 255:
+			steps[i] = -steps[i]
+			RGB[i] = 255
+		if RGB[i] < 0:
+			steps[i] = -steps[i]
+			RGB[i] = 0
 
-	if random.random()>0.999:
-		Bstep = -Bstep
-	B += Bstep
-
-
-	if R>255 or R<0:
-		Rstep = -Rstep
-		if R > 255:
-			R = 255
-		if R < 0:
-			R = 0
-	if G>255 or G<0:
-		Gstep = -Gstep
-		if G > 255:
-			G = 255
-		if G < 0:
-			G = 0
-	if B>255 or B<0:
-		Bstep = -Bstep
-		if B > 255:
-			B = 255
-		if B < 0:
-			B = 0
-
-
-
-	shelf_colours[0] = (R, G, B)
 	sys.stdout.write("\r"+" "*80)
-	sys.stdout.write("\rR,G,B=\t{0}\t{1}\t{2}\tsteps=\t{3}\t{4}\t{5}".format(R,G,B,Rstep,Gstep,Bstep))
+	sys.stdout.write("\rR,G,B=\t{0}\t{1}\t{2}\tsteps=\t{3}\t{4}\t{5}".format(RGB[0],RGB[1],RGB[2],steps[0],steps[1],steps[2]))
 
 	# Propogate the shelf colours:
-	for i in range(len(shelves)-1, -1, -1):
-		shelf_colours[i] = shelf_colours[i-1]
+	for i in range(len(shelves)-1, 0, -1):
+		shelf_colours[i] = tuple(shelf_colours[i-1])
 	set_shelves()
 	time.sleep(0.01)
 
