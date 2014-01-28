4bdb6bb6bec8ee6217c8fc6bfc1584e7dcb2b337
David Turner <cillian64@googlemail.com>
2014-01-28 23:13:12 +0000

    Added long shift register to colourfade

--- loraine/clients/bookshelf_colour_fade.py
+++ loraine/clients/bookshelf_colour_fade.py
@@ -19,15 +19,18 @@ def set_shelves():
 	for i in range(0,len(shelves)):
 		set_shelf(shelves[i], shelf_colours[i])
 
-# Initialise shelf colours:
-for i in range(0,len(shelves)):
-	shelf_colours.append((0,0,0))
-
+# Initialise shelves to black:
+shelf_colours = list([(0,0,0)]*len(shelves))
 set_shelves()
 
-RGB=list((0, 0, 0))
-shelf_colours[0]=RGB
-steps=list((1,1,1))	
+# Setup shift register:
+spacing=25
+shift_reg = list([(0,0,0)]*len(shelves)*spacing)
+
+RGB=list((0,0,0))
+shift_reg[0]=RGB
+steps=list((1,1,1))
+
 while True:
 	for i in range(0,3):
 		if random.random()>0.999:
@@ -41,12 +44,19 @@ while True:
 			steps[i] = -steps[i]
 			RGB[i] = 0
 
-	sys.stdout.write("\r"+" "*80)
-	sys.stdout.write("\rR,G,B=\t{0}\t{1}\t{2}\tsteps=\t{3}\t{4}\t{5}".format(RGB[0],RGB[1],RGB[2],steps[0],steps[1],steps[2]))
+	sys.stdout.write("\r"+" "*80) # Clear line
+	sys.stdout.write("\rR,G,B=\t{0}\t{1}\t{2}\t".format(RGB[0],RGB[1],RGB[2]))
+	sys.stdout.write("steps=\t{0}\t{1}\t{2}".format(steps[0],steps[1],steps[2]))
 
-	# Propogate the shelf colours:
-	for i in range(len(shelves)-1, 0, -1):
-		shelf_colours[i] = tuple(shelf_colours[i-1])
+	# Propogate the shift register:
+	for i in range(len(shift_reg)-1, 0, -1):
+		shift_reg[i] = tuple(shift_reg[i-1])
+
+	# Extract taps:
+	for i in range(0,len(shelves)):
+		shelf_colours[i] = shift_reg[i*spacing]
+
+	# Update shelves:
 	set_shelves()
 	time.sleep(0.01)
 
