6a523f3c7f2e5e2d8308ff0b641a012d422cf5d8
David Turner <cillian64@googlemail.com>
2014-01-29 00:41:30 +0000

    Fixed some code style issues

--- loraine/clients/bookshelf_colour_fade.py
+++ loraine/clients/bookshelf_colour_fade.py
@@ -4,58 +4,57 @@ import random
 import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-oracle_ip = "192.168.2.5"
-oracle_port = 7878
+ORACLE_IP = "192.168.2.5"
+ORACLE_PORT = 7878
 
 shelves = ("one", "two", "three", "four", "five", "six")
-shelf_colours = list()
 colours = ("255,0,0", "0,255,0", "0,0,255", "0,0,0")
 
 def set_shelf(shelf, colour):
-	msg = "bookshelf.{0}={1},{2},{3}".format(shelf,colour[0],colour[1],colour[2])
-	sock.sendto(msg.encode(), (oracle_ip, oracle_port))
+	msg = "bookshelf.{0}={1},{2},{3}".format(shelf, colour[0], colour[1], colour[2])
+	sock.sendto(msg.encode(), (ORACLE_IP, ORACLE_PORT))
 
 def set_shelves():
-	for i in range(0,len(shelves)):
+	for i in range(len(shelves)):
 		set_shelf(shelves[i], shelf_colours[i])
 
 # Initialise shelves to black:
-shelf_colours = list([(0,0,0)]*len(shelves))
+shelf_colours = [[0, 0, 0]] * len(shelves)
 set_shelves()
 
 # Setup shift register:
-spacing=25
-shift_reg = list([(0,0,0)]*len(shelves)*spacing)
+SPACING = 25
+shift_reg = [[0, 0, 0]] * len(shelves) * SPACING
 
-RGB=list((0,0,0))
-shift_reg[0]=RGB
-steps=list((1,1,1))
+rgb = [0, 0, 0]
+shift_reg[0] = rgb
+steps = [1, 1, 1]
 
 while True:
-	for i in range(0,3):
-		if random.random()>0.999:
+	for i in range(3):
+		if random.random() > 0.999:
 			steps[i] = -steps[i]
-		RGB[i] += steps[i]
+		rgb[i] += steps[i]
 
-		if RGB[i] > 255:
+		if rgb[i] > 255:
 			steps[i] = -steps[i]
-			RGB[i] = 255
-		if RGB[i] < 0:
+			rgb[i] = 255
+		if rgb[i] < 0:
 			steps[i] = -steps[i]
-			RGB[i] = 0
+			rgb[i] = 0
 
-	sys.stdout.write("\r"+" "*80) # Clear line
-	sys.stdout.write("\rR,G,B=\t{0}\t{1}\t{2}\t".format(RGB[0],RGB[1],RGB[2]))
-	sys.stdout.write("steps=\t{0}\t{1}\t{2}".format(steps[0],steps[1],steps[2]))
+	sys.stdout.write("\r" + " " * 80) # Clear line
+	sys.stdout.write("\rR,G,B=\t{0}\t{1}\t{2}\t".format(rgb[0], rgb[1], rgb[2]))
+	sys.stdout.write("steps=\t{0}\t{1}\t{2}".format(steps[0], steps[1], steps[2]))
 	sys.stdout.flush()
 
 	# Propogate the shift register:
-	for i in range(len(shift_reg)-1, 0, -1):
-		shift_reg[i] = tuple(shift_reg[i-1])
+	for i in range(len(shift_reg) - 1, 0, -1):
+		shift_reg[i] = tuple(shift_reg[i - 1])
 
 	# Extract taps:
-	for i in range(0,len(shelves)):
-		shelf_colours[i] = shift_reg[i*spacing]
+	for i in range(len(shelves)):
+		shelf_colours[i] = shift_reg[i * SPACING]
 
 	# Update shelves:
 	set_shelves()
