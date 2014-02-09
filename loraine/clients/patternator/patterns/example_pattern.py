cb8c7a7ff500005072b9d1a5a57a072c34e10bea
David Turner <cillian64@googlemail.com>
2014-02-09 19:18:27 +0000

    updated comments

--- loraine/clients/patternator/patterns/example_pattern.py
+++ loraine/clients/patternator/patterns/example_pattern.py
@@ -1,9 +1,13 @@
+# Patternator pattern
+# Does nothing -- example pattern.
+
 def example_pattern(beat, beat_progress, bpm):
     # beat is a zero-based integer which increments upon each beat occuring
     # beat_progress is a float which progresses from 0 to 1 through the
     #   duration of each beat
     # bpm is a float giving current best guess of the current bpm
 
+    # RGB values are integers from 0 to 255 inclusive.
     return {"bookshelf.one":    (0, 0, 0),
             "bookshelf.two":    (0, 0, 0),
             "bookshelf.three":  (0, 0, 0),
