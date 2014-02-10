ca804bad382f59bbaddbd4cdee0bc634a1a659eb
David Turner <cillian64@googlemail.com>
2014-02-10 21:42:43 +0000

    Changed colours slightly in test_inc

--- loraine/clients/test_incandescent.py
+++ loraine/clients/test_incandescent.py
@@ -21,12 +21,23 @@ def set_shelves(colours):
 
 
 def inc_to_rgb(state):
-    rgb = [state]*3
-    rgb[2] = rgb[2] if rgb[2] < 0.8 else 0.8
+    rgb = [float(state)]*3
+
+    # Cap blue and green to give a nice warm white
+    rgb[2] = rgb[2] if rgb[2] < 0.6 else 0.6
+    rgb[1] = rgb[1] if rgb[1] < 0.6 else 0.6
+
+    # Superlinear decay on the blue to give a nice warm decay
     rgb[2] **= 2.5
-    rgb[1] **= 1.5
 
+    # Bodge factors for low values
+    if state > 0.02:
+        rgb[1] **= 1.5
+    else:
+        rgb[1] = state**0.5/50
+        rgb[0] = rgb[1]
 
+    # Rescale RGB:
     rgb = [x*255.0 for x in rgb]
     return rgb
 
@@ -36,13 +47,13 @@ while True:
     beat_progress = time.time() % 1
 
     if beat_progress < 0.2:
-        state += 0.05
+        state += 0.08
     else:
         state /= 1.15
     state = state if 0.0<=state<=1.0 else (1.0 if state>=1.0 else 0.0)
 
     rgb = inc_to_rgb(state)
     rgb = [int(x) for x in rgb]
-    print(str(state)+"\t"+str(rgb[0])+"\t"+str(rgb[1])+"\t"+str(rgb[2]))
+#    print(str(state)+"\t"+str(rgb[0])+"\t"+str(rgb[1])+"\t"+str(rgb[2]))
     set_shelves(rgb)
     time.sleep(0.02)
