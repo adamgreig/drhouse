383288cb8fc7399c96e9feaafd03116ec08a188a
Adam Greig <adam@adamgreig.com>
2014-02-11 01:44:26 +0000

    add first go at loraine python lib

---
+++ loraine/loraine/__init__.py
@@ -0,0 +1,36 @@
+ip = "192.168.2.5"
+port = 7878
+url = "http://{0}/loraine.yaml".format(ip)
+
+import yaml
+import socket
+import requests
+from array import array
+
+addresses = {}
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def fetch(url):
+    global addresses
+    r = requests.get(url)
+    config = yaml.load(r.text)["lights"]
+    for module in config:
+        for light in config[module]:
+            addresses[module + "." + light] = config[module][light]
+
+
+def set_rgb(commands):
+    """commands = [("bookshelf.one", (255, 0, 255)),
+                   ("bookshelf.two", (0, 255, 255)), ..., ]
+    """
+    arr = array('B')
+    for command in commands:
+        addrs = addresses[command[0]]
+        r, g, b = addrs['red'], addrs['green'], addrs['blue']
+        arr.extend((r, command[1][0], g, command[1][1], b, command[1][2]))
+    print(arr.tolist())
+    sock.sendto(arr.tobytes(), (ip, port))
+
+
+fetch(url)
