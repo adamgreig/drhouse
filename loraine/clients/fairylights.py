2956a7bb1a624b8c377510a2888bf80c8ebf6b8b
Adam Greig <adam@adamgreig.com>
2014-03-31 02:13:11 +0100

    add auto lights

--- loraine/clients/fairylights.py
+++ loraine/clients/fairylights.py
@@ -1,6 +1,8 @@
 import sys
 import time
+import astral
 import loraine
+import datetime
 
 lights = ["fairylights.back", "fairylights.middle", "fairylights.front"]
 
@@ -21,12 +23,32 @@ def fade_off():
         time.sleep(0.05)
 
 
+def auto():
+    """Call at xx15 and xx45 each hour please."""
+    city = astral.Astral()['london']
+    now_dt = datetime.datetime.now()
+    sunrise_tt = city.sun(date=now_dt, local=False)['sunset'].timetuple()
+    sunrise_t = datetime.time(sunrise_tt.tm_hour, sunrise_tt.tm_min,
+                              sunrise_tt.tm_sec)
+    sunrise_dt = datetime.datetime.combine(datetime.date.today(), sunrise_t)
+
+    delta = (now_dt - sunrise_dt).total_seconds()
+
+    if 0.0 <= delta <= 1800.0:
+        fade_on()
+
+    if now_dt.hour == 3 and now_dt.minute < 30:
+        fade_off()
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         if sys.argv[1] == "on":
             fade_on()
         elif sys.argv[1] == "off":
             fade_off()
+        elif sys.argv[1] == "auto":
+            auto()
     else:
         fade_on()
         time.sleep(3)
